1a2cf8876d15de339037a0c598c23fae309724e7
Lux <fails9503@gmail.com>
2022-07-24 12:22:33 +0900

    feat. Private Repository 생성

---
+++ src/main/java/com/example/throwingmoney/ThrowingMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.throwingmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrowingMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThrowingMoneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/throwingmoney/ThrowingMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.throwingmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrowingMoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
